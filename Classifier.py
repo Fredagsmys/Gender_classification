cb11fd110eb1e6707ea08c03f7d05bbb657066c2
Fredagsmys <max.mattsson00@gmail.com>
2023-02-17 10:13:42 +0100

    test

--- Classifier.py
+++ Classifier.py
@@ -5,11 +5,16 @@ import sklearn.preprocessing as skl_pre
 import sklearn.linear_model as skl_lm
 import sklearn.discriminant_analysis as skl_da
 import sklearn.neighbors as skl_nb
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import MinMaxScaler as MMScaler
 
 def choose_data(df):
-    newdf = pd.DataFrame({"percent words female": df['Number words female']/df['Total words'],
-                        "percent words male": df['Number words male']/df['Total words'],
+    newdf = pd.DataFrame({
+                        # "percent words female": df['Number words female']/df['Total words'],
+                        # "percent words male": df['Number words male']/df['Total words'],
+                        "Number words female": df['Number words female'],
+                        "Number words male": df['Number words male'],
+                        "Total words": df['Total words'],
                         "Number of words lead": df['Number of words lead'],
                         "Difference in words lead and co-lead":df['Difference in words lead and co-lead'],
                         "Number of male actors":df['Number of male actors'],
@@ -22,43 +27,43 @@ def choose_data(df):
                         "Age Co-Lead":df['Age Co-Lead'],
                         "Lead":df['Lead'],
                         })
-
-
     return newdf
+
+
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+# plt.plot(train_data['Lead'])
+
+X_test = test_data
+#==============DATA ANALYSIS=============
+
+
+
+
+
 
 
 scaler = MMScaler()
 
 
-train_data = pd.read_csv('train.csv')
-test_data = pd.read_csv('test.csv')
-X_test = test_data
-print(train_data.info())
-print(choose_data(train_data).info())
 train_data = choose_data(train_data)
-
-# Splitting dataset
-
-
-
-
-np.random.seed(1)
-trainI = np.random.choice(train_data.shape[0], size=train_data.shape[0]-200, replace=False)
-trainIndex = train_data.index.isin(trainI)
-train = train_data.iloc[trainIndex]
-validation = train_data.iloc[~trainIndex]
-X_train = np.array(train.loc[:, train.columns != 'Lead'])
-y_train = np.array(train['Lead'])
-X_val = np.array(validation.loc[:, train.columns != 'Lead'])
-y_val = np.array(validation['Lead'])
-#end
+print(train_data['Number words female'])
+print(train_data['Number words male'])
+print(train_data['Total words'])
+# print(train_data['Lead'].describe())
+X_train = np.array(train_data.loc[:, train_data.columns != 'Lead'])
+y_train = np.array(train_data['Lead'])
 
 
-test_data = scaler.fit_transform(X_test)
-train_data = scaler.fit_transform(X_train)
-X_val = scaler.fit_transform(X_val)
+# normalized data
+# X_test = scaler.fit_transform(X_test)
+# X_train = scaler.fit_transform(X_train)
+# X_val = scaler.fit_transform(X_val)
 
-
+# non-noralized data
+# test_data = X_test
+# train_data = X_train
+# X_val = X_val
 
 
 
@@ -66,21 +71,14 @@ X_val = scaler.fit_transform(X_val)
 
 
 LDA = skl_da.LinearDiscriminantAnalysis()
-LDA.fit(X_train, y_train)
-
-prediction = LDA.predict(X_val)
-err = np.mean(prediction == y_val)
-confusion = pd.crosstab(prediction,y_val)
 print("\n=================LDA=================")
-print(f"Confusion matrix:\n{confusion}")
-print(f"\nError on validation set with LDA: {1-err}")
+scores = cross_val_score(LDA,X_train,y_train,cv=5,scoring="accuracy")
+print(scores.mean())
 
 print("\n=================QDA=================")
 QDA = skl_da.QuadraticDiscriminantAnalysis()
-QDA.fit(X_train, y_train)
-
-prediction = QDA.predict(X_val)
-err = np.mean(prediction == y_val)
-confusion = pd.crosstab(prediction,y_val)
-print(f"Confusion matrix:\n{confusion}")
-print(f"\nError on validation set with QDA: {1-err}")
+scoresQDA = cross_val_score(QDA,X_train,y_train,cv=5,scoring="accuracy")
+QDA.fit(X_train,y_train)
+# print(QDA.predict(X_test))
+print(scoresQDA.mean())
+scoresQDA
